38793820d3e6ff71fd32504e3490d1d89be0d930
Miguel Gamboa <miguelgamboa@outlook.com>
2026-04-24 17:14:45 +0100

    Class-File API

--- lesson22-class-file-api/src/main/kotlin/pt/isel/App.kt
+++ lesson22-class-file-api/src/main/kotlin/pt/isel/App.kt
@@ -13,14 +13,10 @@ import java.lang.constant.ConstantDescs.CD_void
 import java.lang.constant.ConstantDescs.INIT_NAME
 import java.lang.constant.ConstantDescs.MTD_void
 import java.lang.constant.MethodTypeDesc
-import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
-import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.jvm.isAccessible
 
 /*
- public final class pt.isel.Bar {
-  public pt.isel.Bar();
+public pt.isel.Bar();
     Code:
        0: aload_0
        1: invokespecial #8                  // Method java/lang/Object."<init>":()V
@@ -36,16 +32,23 @@ interface IFoo {
     fun foo(): Int
 }
 
-fun buildClassBar() {
+interface Sum {
+    fun add(other: Int): Int
+}
+
+class CounterBaseline(
+    private val nr: Int,
+) : Sum {
+    override fun add(other: Int): Int = nr + other
+}
+
+fun buildBar() {
     val className = "pt.isel.Bar"
-    // val ifooDesc = ClassDesc("pt.isel.IFoo")
     val ifooDesc = ClassDesc.of(IFoo::class.qualifiedName)
     val bytes: ByteArray =
         ClassFile.of().build(ClassDesc.of(className)) { clb ->
             clb
-                .withFlags(ACC_PUBLIC or ACC_FINAL)
                 .withInterfaces(Interfaces.ofSymbols(ifooDesc).interfaces())
-                // .withMethod(INIT_NAME, MethodTypeDesc.of(CD_Void)) { mb: MethodBuilder ->
                 .withMethod(INIT_NAME, MTD_void, ACC_PUBLIC) { mb ->
                     mb.withCode { cob ->
                         cob
@@ -53,7 +56,7 @@ fun buildClassBar() {
                             .invokespecial(CD_Object, INIT_NAME, MTD_void)
                             .return_()
                     }
-                }.withMethod("foo", MethodTypeDesc.of(CD_int), ACC_PUBLIC) { mb ->
+                }.withMethod("foo", MethodTypeDesc.of(CD_int), ACC_PUBLIC or ACC_FINAL) { mb ->
                     mb.withCode { cob ->
                         cob
                             .ldc(clb.constantPool().intEntry(67895))
@@ -66,74 +69,33 @@ fun buildClassBar() {
             .getResource("/")
             ?.toURI()
             ?.path
-
     File(resourcePath, className.replace('.', '/') + ".class")
         .also { it.parentFile.mkdirs() } // Create directories if they do not exist
         .writeBytes(bytes)
 }
 
-fun main() {
-    buildClassBar()
-    val barKlass: KClass<out Any> =
-        Unit::class.java.classLoader
-            .loadClass("pt.isel.Bar")
-            .kotlin
-    // val foo = barKlass.declaredFunctions.first { it.name == "foo" }
-    // println("foo() = " + foo.call(barKlass.createInstance()))
-    val bar = barKlass.createInstance() as IFoo
-    println("foo() = ${bar.foo()}")
-
-    buildCounter()
-    val counterKlass =
-        Unit::class.java
-            .classLoader
-            .loadClass("pt.isel.Counter")
-            .kotlin
-    val counter =
-        counterKlass
-            .constructors
-            .first()
-            .call(17) as Sum
-    counter.add(11)
-    val nrValue =
-        counterKlass
-            .declaredMemberProperties
-            .first { it.name == "nr" }
-            .also { it.isAccessible = true }
-            .call(counter)
-    println(nrValue)
-}
-
-interface Sum {
-    fun add(other: Int): Int
-}
-
 /*
-private final int nr;
-
-  public pt.isel.CounterBaseline(int);
-    Code:
-
-  public final int add(int);
-    Code:
- */
+public final class pt.isel.CounterBaseline implements pt.isel.Sum {
+*/
 fun buildCounter() {
     val className = "pt.isel.Counter"
-    val sum = ClassDesc.of(Sum::class.qualifiedName)
+    val sumDesc = ClassDesc.of(Sum::class.qualifiedName)
     val bytes: ByteArray =
         ClassFile.of().build(ClassDesc.of(className)) { clb ->
             clb
-                .withFlags(ACC_PUBLIC or ACC_FINAL)
-                .withInterfaces(Interfaces.ofSymbols(sum).interfaces())
-                .withField("nr", CD_int, ACC_PRIVATE)
+                .withInterfaces(Interfaces.ofSymbols(sumDesc).interfaces())
+                //  private final int nr;
+                .withField("nr", CD_int, ACC_PRIVATE or ACC_FINAL)
+                //   public pt.isel.CounterBaseline(int);
+                //    Code:
+                //       0: aload_0
+                //       1: invokespecial #11                 // Method java/lang/Object."<init>":()V
+                //       4: aload_0
+                //       5: iload_1
+                //       6: putfield      #15                 // Field nr:I
+                //       9: return
                 .withMethod(INIT_NAME, MethodTypeDesc.of(CD_void, CD_int), ACC_PUBLIC) { mb ->
                     mb.withCode { cob ->
-                        // 0: aload_0
-                        // 1: invokespecial #11                 // Method java/lang/Object."<init>":()V
-                        // 4: aload_0
-                        // 5: iload_1
-                        // 6: putfield      #15                 // Field nr:I
-                        // 9: return
                         cob
                             .aload(0)
                             .invokespecial(CD_Object, INIT_NAME, MTD_void)
@@ -142,17 +104,21 @@ fun buildCounter() {
                             .putfield(ClassDesc.of(className), "nr", CD_int)
                             .return_()
                     }
-                }.withMethod("add", MethodTypeDesc.of(CD_int, CD_int), ACC_PUBLIC) { mb ->
+                }
+                //   public int add(int);
+                //    Code:
+                //       0: aload_0
+                //       1: getfield      #15                 // Field nr:I
+                //       4: iload_1
+                //       5: iadd
+                //       6: ireturn
+                .withMethod("add", MethodTypeDesc.of(CD_int, CD_int), ACC_PUBLIC or ACC_FINAL) { mb ->
                     mb.withCode { cob ->
                         cob
                             .aload(0)
-                            .dup()
                             .getfield(ClassDesc.of(className), "nr", CD_int)
                             .iload(1)
                             .iadd()
-                            .putfield(ClassDesc.of(className), "nr", CD_int)
-                            .aload(0)
-                            .getfield(ClassDesc.of(className), "nr", CD_int)
                             .ireturn()
                     }
                 }
@@ -162,8 +128,32 @@ fun buildCounter() {
             .getResource("/")
             ?.toURI()
             ?.path
-
     File(resourcePath, className.replace('.', '/') + ".class")
         .also { it.parentFile.mkdirs() } // Create directories if they do not exist
         .writeBytes(bytes)
+}
+
+fun main() {
+    buildBar()
+
+    val barKlass =
+        Unit::class.java.classLoader
+            .loadClass("pt.isel.Bar")
+            .kotlin
+    val bar = barKlass.createInstance() as IFoo
+
+    // val res = barKlass.memberFunctions.first { it.name == "foo" }.call(bar)
+    // println(res)
+
+    // Alternative AVOIDING Reflect
+    println(bar.foo())
+
+    buildCounter()
+    val counterKlass =
+        Unit::class.java.classLoader
+            .loadClass("pt.isel.Counter")
+            .kotlin
+    val counter = counterKlass.constructors.first().call(23) as Sum
+    println(counter.add(11))
+
 }
